fc5f74831ab94189eb80e458d0ea3d693aee76c2
lucasjdr <lucasjdelrio@hotmail.com>
2020-02-25 16:28:41 -0300

    Getting auth info

---
+++ UWPRedditClient/Interfaces/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UWPRedditClient.Entities;
+
+namespace UWPRedditClient.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<AuthInfo> Authenticate(string username, string password, string client_id, string client_secret);
+    }
+}
