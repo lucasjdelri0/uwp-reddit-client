a7d058de346cb44a4355728de470ac0f032cd885
lucasjdr <lucasjdelrio@hotmail.com>
2020-02-25 15:44:22 -0300

    Auth entities created

---
+++ UWPRedditClient/Entities/AuthInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPRedditClient.Entities
+{
+    public class AuthInfo
+    {
+        public string accessToken { get; set; }
+        public string tokenType { get; set; }
+        public DateTime expiresIn { get; set; }
+        public string baseAddress { get; set; }
+    }
+}
